512daaf34c3502edf38beadec180f97e5de4fc0b
Akshat Kumar <akshatkumar4433@gmail.com>
2022-03-16 21:27:13 -0400

    views

--- app.js
+++ app.js
@@ -34,12 +34,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-/*
-app.use(session({sercet:'ilovemyselfmyselfmyselfmyself'}));
+
+app.use(session({secret:'ilovemyselfmyselfmyselfmyself'}));
+
 app.use(passport.initialize());
 app.use(passport.session());
+
 app.use(flash());
-*/
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,40 @@
 const express = require('express');
 const router = express.Router();
-const login = require('./login')
-const profile = require('./profile')
+var flash = require('connect-flash')
+
 
 /* GET home page. */
 module.exports = () => {
-  router.get('/',login);
-  //router.get('/profile', profile)
-  return router
+  router.get('/', function(req, res, next) {
+    res.render('layout',
+    {
+     template:'login',
+     title: 'login',
+     message:req.flash('loginMessage')
+    })
+  });
+  router.get('/signup', function(req,res,next) {
+    res.render('layout',{template:'signup',title: 'signup',
+     message: req.flash('signupMessage')})
+  })
+
+
+  router.get('/profile', isLoggedIn, function(req,res) {
+    res.render('layout',{template:'profile',title: 'profile',
+        user:req.user})
+  });
+
+  router.get('/logout', function(req,res) {
+    req.logout();
+    res.redirect('/')
+  })
+
+    return router
+}
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticaed())
+    return next()
+
+  res.redirect()
 }
